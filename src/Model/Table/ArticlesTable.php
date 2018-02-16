c31f8bdcb889548442d3309974812492b0ba1562
saiwolf <saiwolf@swmnu.net>
2018-02-15 21:06:03 -0500

    Finished Validation and added the D in CRUD.

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -6,6 +6,9 @@ use Cake\ORM\Table;
 // the Text class
 use Cake\Utility\Text;
 
+// Import the Validator class
+use Cake\Validation\Validator;
+
 class ArticlesTable extends Table
 {
   public function initialize(array $config)
@@ -21,6 +24,19 @@ class ArticlesTable extends Table
       $entity->slug = substr($sluggedTitle, 0, 191);
       
     }
+  }
+  
+  // Our validation method
+  public function validationDefault(Validator $validator)
+  {
+    $validator
+      ->notEmpty('title')
+      ->minLength('title', 10)
+      ->maxLength('title', 255)
+      ->notEmpty('body')
+      ->minLength('body', 10);
+    
+    return $validator;
   }
   
 }
